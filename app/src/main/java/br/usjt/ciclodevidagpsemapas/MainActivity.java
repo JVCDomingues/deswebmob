104f25d3f4bbbcd49b8b73054ed26e54c0a5aff7
João Victor C. Domingues <nperdoo5@gmail.com>
2019-04-10 22:55:25 -0300

    Versão final

--- app/src/main/java/br/usjt/ciclodevidagpsemapas/MainActivity.java
+++ app/src/main/java/br/usjt/ciclodevidagpsemapas/MainActivity.java
@@ -1,3 +1,4 @@
+
 package br.usjt.ciclodevidagpsemapas;
 
 import android.Manifest;
@@ -7,25 +8,32 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.function.Consumer;
+
 public class MainActivity extends AppCompatActivity {
-    private static final int REQUEST_CODE_GPS = 1001;
+
     private TextView locationTextView;
+
+    public static final int REQUEST_CODE_GPS = 1001;
+
+    private LocalizacaoDAO localizacaoDAO;
     private LocationManager locationManager;
     private LocationListener locationListener;
+
     private double latitudeAtual;
     private double longitudeAtual;
 
@@ -36,45 +44,72 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        localizacaoDAO = new LocalizacaoDAO(this);
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Uri gmmIntentUri =
-                        Uri.parse(String.format("geo:%f,%f?q=restaurantes",
-                                latitudeAtual, longitudeAtual));
-                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
-                mapIntent.setPackage("com.google.android.apps.maps");
-                startActivity(mapIntent);
+                Intent intent = new Intent(MainActivity.this, ListaLocaisActivity.class);
+                startActivity(intent);
             }
         });
 
-        locationManager = (LocationManager)
-                getSystemService(Context.LOCATION_SERVICE);
-
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         locationListener = new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
-                double lat = location.getLatitude();
-                double lon = location.getLongitude();
-                latitudeAtual = lat;
-                longitudeAtual = lon;
-                locationTextView.setText(String.format("Lat: %f, Long: %f", lat,
-                        lon));
-            }
-            @Override
-            public void onStatusChanged(String provider, int status, Bundle
-                    extras) {
-            }
-            @Override
-            public void onProviderEnabled(String provider) {
-            }
-            @Override
-            public void onProviderDisabled(String provider) {
+                Localizacao localizacao = new Localizacao(location.getLatitude(), location.getLongitude());
+                locationTextView.setText(String.format("Lat: %f, Long: %f", localizacao.getLatitude(), localizacao.getLongitude()));
+                localizacaoDAO.insertLocalizacao(localizacao);
             }
 
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
         };
         locationTextView = findViewById(R.id.locationTextView);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, APPConstants.REQUEST_LOCATION_UPDATE_MIN_TIME, APPConstants.REQUEST_LOCATION_UPDATE_MIN_DISTANCE, locationListener);
+        } else {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE_GPS);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        locationManager.removeUpdates(locationListener);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CODE_GPS) {
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, APPConstants.REQUEST_LOCATION_UPDATE_MIN_TIME, APPConstants.REQUEST_LOCATION_UPDATE_MIN_DISTANCE, locationListener);
+                }
+            } else {
+                Toast.makeText(this, getString(R.string.no_gps_no_app), Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     @Override
@@ -98,55 +133,4 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        //a permissão já foi dada?
-        if (ActivityCompat.checkSelfPermission(
-                this, Manifest.permission.ACCESS_FINE_LOCATION) ==
-                PackageManager.PERMISSION_GRANTED) {
-            //somente ativa
-            //a localização é obtida via hardware, intervalo de 0 segundos e 0 metros entre atualizações
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
-                    0, 0, locationListener);
-        }
-        else{
-            //permissão ainda não foi nada, solicita ao usuário
-            //quando o usuário responder, o método onRequestPermissionsResult vai ser chamado
-            ActivityCompat.requestPermissions(this,
-                    new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE_GPS);
-        }
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        locationManager.removeUpdates(locationListener);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull
-            String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode == REQUEST_CODE_GPS) {
-            if (grantResults.length > 0 && grantResults[0] ==
-                    PackageManager.PERMISSION_GRANTED){
-                //permissão concedida, ativamos o GPS
-                if (ActivityCompat.checkSelfPermission(this,
-                        Manifest.permission.ACCESS_FINE_LOCATION) ==
-                        PackageManager.PERMISSION_GRANTED) {
-                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
-                            0, 0, locationListener);
-                }
-            }
-            else{
-                //usuário negou, não ativamos
-                Toast.makeText(this, getString(R.string.no_gps_no_app),
-                        Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-
-    //@Override
-
 }
